0af0128d7ca741b4b97de38b7d46e0f1bdbd0f56
ukkari6 <me3t0m@gmail.com>
2025-03-31 02:07:11 +0900

    ソフトシリアル追加

--- mainMPU/mainMPU.ino
+++ mainMPU/mainMPU.ino
@@ -3,8 +3,16 @@
 #include <Arduino.h>
 #include "key_table.h"  // key_table.hをインクルード
 #include <EEPROM.h>
+#include <SoftwareSerial.h>
 
-#define EEPROM_SIZE 512  // EEPROMのサイズ（バイト単位）、ESP32で必要
+#define EEPROM_SIZE 1024  // EEPROMのサイズ（バイト単位）、ESP32で必要
+
+// ソフトウェアシリアルのTX/RXピンの設定
+#define RX_PIN 16    // 受信ピン
+#define TX_PIN 17    // 送信ピン
+
+// SoftwareSerialオブジェクトの作成
+SoftwareSerial softSerial(RX_PIN, TX_PIN);
 
 const int ledPin = 2;  // LEDが接続されているピン番号
 uint8_t boot_eeprom_address_L = 0; // EEPROMのアドレス、下位
@@ -19,7 +27,7 @@ void bootCounter(){
   byte highByte = EEPROM.read(boot_eeprom_address_H); // 上位バイト
 
   // 16ビット値を結合
-  Serial.print("2\n");
+  Serial.print("3\n");
   uint16_t boot_eeprom_counter = (highByte << 8) | lowByte;
 
   if(boot_eeprom_counter >= 1023){
@@ -65,6 +73,10 @@ void setup() {
   pinMode(ledPin, OUTPUT); // LEDピンを出力モードに設定
   // EEPROMの初期化、ESP32で必要
   EEPROM.begin(EEPROM_SIZE);
+
+  // ソフトウェアシリアル通信を初期化
+  softSerial.begin(115200);  // ボーレートは必要に応じて変更
+
 }
 
 //Serial.println(key_table[i]);
@@ -75,6 +87,8 @@ void loop() {
 
   while(1){
     bootCounter_read(); //EEPROMのブート回数を表示する
+
+    softSerial.write("Soft Serial Test\n");
     delay(1000);
   }
 }

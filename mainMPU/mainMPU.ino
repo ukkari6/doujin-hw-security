060b739839cb05fa339064dff070d41a15825340
ukkari6 <me3t0m@gmail.com>
2025-03-30 23:13:09 +0900

    ESP32向けに書き直し、LEDピン変更

--- mainMPU/mainMPU.ino
+++ mainMPU/mainMPU.ino
@@ -1,9 +1,12 @@
+//疑似メインMPU、ESP32-WROOM-32
+
 #include <Arduino.h>
 #include "key_table.h"  // key_table.hをインクルード
 #include <EEPROM.h>
 
+#define EEPROM_SIZE 512  // EEPROMのサイズ（バイト単位）、ESP32で必要
 
-const int ledPin = 13;  // LEDが接続されているピン番号（通常は13番ピン）
+const int ledPin = 2;  // LEDが接続されているピン番号
 uint8_t boot_eeprom_address_L = 0; // EEPROMのアドレス、下位
 uint8_t boot_eeprom_address_H= 1; // EEPROMのアドレス、上位
 
@@ -11,10 +14,12 @@ uint8_t boot_eeprom_address_H= 1; // EEPROMのアドレス、上位
 //1023までいったら0にリセット
 void bootCounter(){
   // EEPROMから2バイト読み込む
+  Serial.print("1\n");
   byte lowByte = EEPROM.read(boot_eeprom_address_L);  // 下位バイト
   byte highByte = EEPROM.read(boot_eeprom_address_H); // 上位バイト
 
   // 16ビット値を結合
+  Serial.print("2\n");
   uint16_t boot_eeprom_counter = (highByte << 8) | lowByte;
 
   if(boot_eeprom_counter >= 1023){
@@ -24,12 +29,20 @@ void bootCounter(){
   }
 
   // 上位バイトと下位バイトに分けてEEPROMに保存
+  Serial.print("3\n");
   byte lowByteNew = boot_eeprom_counter & 0xFF; // 下位バイト
   byte highByteNew = (boot_eeprom_counter >> 8) & 0xFF; // 上位バイト
 
   // 新しい値をEEPROMに保存
+  Serial.print("4\n");
   EEPROM.write(boot_eeprom_address_L, lowByteNew);  // 下位バイト
   EEPROM.write(boot_eeprom_address_H, highByteNew); // 上位バイト
+
+  // EEPROMへの書き込みを確定
+  EEPROM.commit();
+
+  Serial.print("5\n");
+
 }
 
 //EEPROMの内容をシリアルに送信
@@ -48,13 +61,18 @@ void bootCounter_read(){
 
 
 void setup() {
-  Serial.begin(9600);      // シリアル通信の初期化
+  Serial.begin(115200);      // シリアル通信の初期化
   pinMode(ledPin, OUTPUT); // LEDピンを出力モードに設定
+  // EEPROMの初期化、ESP32で必要
+  EEPROM.begin(EEPROM_SIZE);
 }
 
+//Serial.println(key_table[i]);
 void loop() {
   delay(1000);
   bootCounter();  //ブート回数をEEPROMに保存
+  //bootCounter_read(); //EEPROMのブート回数を表示する
+
   while(1){
     bootCounter_read(); //EEPROMのブート回数を表示する
     delay(1000);
